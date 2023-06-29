2475a12652139479ccc7758e07161f048a61e8db
ShravyaPoojari <137195758+ShravyaPoojari@users.noreply.github.com>
2023-06-29 20:28:34 +0530

    Add files via upload

---
+++ python_assgn1a).py
@@ -0,0 +1,9 @@
+# value of pi
+pi = 3.14159265
+#taking input for radius of the circle
+#radius is in the form of decimal i.e floating point 
+r = float(input("Enter the radius of the circle:"))
+#Formula to calculate the area of the cirlce
+Area = pi*r*r
+#To get the area of the circle with radius 1.1
+print("The area of the circle with radius 1.1 is:",Area)
