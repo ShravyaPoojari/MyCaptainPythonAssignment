e1539f9844982ac06626292b4bb55dbc5ce7140a
ShravyaPoojari <137195758+ShravyaPoojari@users.noreply.github.com>
2023-06-29 20:42:05 +0530

    Add files via upload

---
+++ python_assgn1.py
@@ -0,0 +1,14 @@
+# value of pi
+pi = 3.14159265
+#taking input for radius of the circle
+#radius is in the form of decimal i.e floating point 
+r = float(input("Enter the radius of the circle:"))
+#Formula to calculate the area of the cirlce
+Area = pi*r*r
+#To get the area of the circle with radius 1.1
+print("The area of the circle with radius 1.1 is:",Area)
+#taking a pre saved file as input
+input("Input the file name:")
+#name of the file
+#specifying the extension type of the file i.e py
+print(input("The extension of the file is:"))
